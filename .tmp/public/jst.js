e5e816ec21067c6754d073d16014f5d8dbd420f1
Travis Czerw <travis.czerw@gmail.com>
2015-03-05 11:53:48 -0600

    Fixed a typo in login.html that was making link to register vanish

--- .tmp/public/jst.js
+++ .tmp/public/jst.js
@@ -34,7 +34,7 @@ this["JST"]["assets/templates/login.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<section ng-controller="loginCtrl">\n\t<header>\n\t\t<h1> Login </h1>\n\t</header>\n\t<form ng-submit="loginSubmit(username, userpassword)">\n\t\t<input ng-model="username" type="text" placeholder="Please enter your username"> \n\t\t<input ng-model="userpassword" type="Password" placeholder="password">\n\t\t<button> Log In </button>\n\t</form>\n\t<span ng-show="emailErrorMsg"> That is not a valid email </span>\n\t<span ng-show="passwordErrorMsg"> That is not a valid password </span>\n\t<span ng-bind="loginErrorArray"></div>\n\t<div>\n\t\t<a ui-sref="register"> Sign Up for Iron Works </a>\n\t</div>\n</section>';
+__p += '<section ng-controller="loginCtrl">\n\t<header>\n\t\t<h1> Login </h1>\n\t</header>\n\t<form ng-submit="loginSubmit(username, userpassword)">\n\t\t<input ng-model="username" type="text" placeholder="Please enter your username"> \n\t\t<input ng-model="userpassword" type="Password" placeholder="password">\n\t\t<button> Log In </button>\n\t</form>\n\t<span ng-show="emailErrorMsg"> That is not a valid email </span>\n\t<span ng-show="passwordErrorMsg"> That is not a valid password </span>\n\t<span ng-bind="loginErrorArray"></span>\n\t<div>\n\t\t<a ui-sref="register"> Sign Up for Iron Works </a>\n\t</div>\n</section>';
 
 }
 return __p
