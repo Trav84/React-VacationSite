d526a1f7567cd2966158754bd2766bca24beed25
Travis Czerw <travis.czerw@gmail.com>
2015-04-11 15:40:56 -0500

    Setting up model and controller

---
+++ api/controllers/TripController.js
@@ -0,0 +1,11 @@
+/**
+ * TripController
+ *
+ * @description :: Server-side logic for managing Trips
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
